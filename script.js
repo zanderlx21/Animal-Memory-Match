6cf5f911f91f0935dea7760af154fb24f616d3be
kirkwood616 <kirkwood616@gmail.com>
2021-12-07 20:27:06 -0500

    updated timers, reset, assests folder, restructured script

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// CARD DECK ARRAY
 let cardArray = [
   { name: "crocodile", value: 1, img: "assets/crocodile.png" },
   { name: "crocodile", value: 1, img: "assets/crocodile.png" },
@@ -13,8 +14,7 @@ let cardArray = [
   { name: "tiger", value: 6, img: "assets/tiger.png" },
 ];
 
-// cardArray.sort(() => 0.5 - Math.random());
-
+// ELEMENT VARIABLES
 const start = document.getElementById("start");
 const startBtn = document.getElementById("startBtn");
 const main = document.getElementById("main");
@@ -22,78 +22,62 @@ const grid = document.createElement("section");
 const startContainer = document.getElementById("startContainer");
 const header = document.getElementById("header");
 const movesCounter = document.getElementById("moves");
+const scene = document.querySelector(".scene");
+const cardFront = document.querySelector(".cardFront");
+const cardBack = document.querySelector(".cardBack");
 
-totalClicks = 0;
-
-grid.setAttribute("class", "grid");
-
-main.appendChild(grid);
-
-startBtn.addEventListener("click", () => {
-  start.remove();
-  startContainer.remove();
-  header.style.display = "flex";
-});
-
-function reset() {
-  location.reload();
-}
-
-// TIMER
+// TIMER VARIABLES
 let second = 0;
 let minute = 0;
 let timer = document.querySelector("#timer");
 let matchedCount = 0;
 let interval;
 
-function startTimer() {
-  interval = setInterval(function () {
-    if (second < 10) {
-      timer.innerHTML = minute + ":0" + second;
-    } else {
-      timer.innerHTML = minute + ":" + second;
-    }
-    second++;
-    if (second === 60) {
-      minute++;
-      second = 0;
-    }
-    if (minute === 60) {
-      hour++;
-      minute = 0;
-    }
-  }, 1000);
-}
-
-//MOVES
-let moves = 0;
-
-// CREATE CARDS
-const scene = document.querySelector(".scene");
-const cardFront = document.querySelector(".cardFront");
-const cardBack = document.querySelector(".cardBack");
-
-cardArray.forEach((item) => {
-  // CARD
-  const card = document.createElement("div");
-  card.classList.add("card");
-  card.dataset.name = item.value;
-  grid.append(card);
-  // CARD FRONT
-  const card2 = document.createElement("div");
-  card2.classList.add("cardFront");
-  card2.style.backgroundImage = `url(${item.img})`;
-  card.appendChild(card2);
-  // CARD BACK
-  const card3 = document.createElement("div");
-  card3.classList.add("cardBack");
-  card.appendChild(card3);
-});
-
+// GUESS VARIABLES
 let counter = 0;
 let firstGuess = 0;
 let secondGuess = 0;
 
+// CLICK VARIABLES
+let totalClicks = 0;
+let moves = 0;
+
+// TOTAL GAME TIME VARIABLE
+let finalTime = 0;
+
+// START SCREEN BUTTON
+startBtn.addEventListener("click", () => {
+  start.remove();
+  deal();
+  startContainer.remove();
+  header.style.display = "flex";
+});
+
+// CREATE CARDS FUNCTION
+function deal() {
+  grid.setAttribute("class", "grid");
+  main.appendChild(grid);
+  cardArray.sort(() => 0.5 - Math.random());
+  // ^^^^ SHUFFLE RANDOMIZER ^^^^
+  cardArray.forEach((item) => {
+    // CARD
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.dataset.name = item.value;
+    grid.append(card);
+    // CARD FRONT (COVERED)
+    const card2 = document.createElement("div");
+    card2.classList.add("cardFront");
+    card2.style.backgroundImage = `url(${item.img})`;
+    card.appendChild(card2);
+    // CARD BACK (SHOWING)
+    const card3 = document.createElement("div");
+    card3.classList.add("cardBack");
+    card.appendChild(card3);
+  });
+}
+
+// CARD MATCH CHECK FUNCTIONS
 const match = () => {
   let selected = document.querySelectorAll(".selected");
   grid.classList.add("lock");
@@ -104,7 +88,7 @@ const match = () => {
     setTimeout(() => {
       grid.classList.remove("lock");
       card.classList.add("match");
-    }, 1000);
+    }, 700);
   });
   firstGuess = 0;
   secondGuess = 0;
@@ -121,13 +105,40 @@ const noMatch = () => {
     setTimeout(() => {
       card.classList.remove("selected");
       grid.classList.remove("lock");
-    }, 1000);
+    }, 700);
   });
   firstGuess = 0;
   secondGuess = 0;
   counter = 0;
 };
 
+// END OF GAME FUNCTION
+function gameEnd() {
+  if (matchedCount === 6) {
+    clearInterval(interval);
+    finalTime = timer.innerHTML;
+    console.log(finalTime);
+    let pTime = document.querySelector(".pTime");
+    let pMoves = document.querySelector(".pMoves");
+    let winningTime = `${minute} minutes & ${second} seconds`;
+    let winningMoves = `${moves} moves`;
+    if (minute < 1) {
+      winningTime = `${second} seconds`;
+    } else if (minute === 1) {
+      winningTime = `${minute} minute & ${second} seconds`;
+    }
+    pTime.innerHTML = winningTime;
+    pMoves.innerHTML = winningMoves;
+    let winWindow = document.querySelector(".winWindow");
+    setTimeout(() => {
+      winWindow.style.display = "flex";
+    }, 1000);
+  } else {
+    return;
+  }
+}
+
+// MAIN GAME LOGIC
 grid.addEventListener("click", function (e) {
   const clicked = e.target;
   if (
@@ -163,29 +174,47 @@ grid.addEventListener("click", function (e) {
   }
 });
 
-let finalTime = 0;
+// RESET BOARD FUNCTION
+function reset() {
+  const cards = document.querySelectorAll(".card");
+  totalClicks =
+    second =
+    minute =
+    matchedCount =
+    moves =
+    counter =
+    firstGuess =
+    secondGuess =
+    finalTime =
+      0;
+  movesCounter.innerHTML = "Moves: " + moves;
+  clearInterval(interval);
+  timer.innerHTML = "0:00";
+  cards.forEach((card) => {
+    card.remove();
+  });
+  grid.remove();
+  deal();
+  let winWindow = document.querySelector(".winWindow");
+  winWindow.style.display = "none";
+}
 
-function gameEnd() {
-  if (matchedCount === 6) {
-    clearInterval(interval);
-    finalTime = timer.innerHTML;
-    console.log(finalTime);
-    let pTime = document.querySelector(".pTime");
-    let pMoves = document.querySelector(".pMoves");
-    let winningTime = `${minute} minutes & ${second} seconds`;
-    let winningMoves = `${moves} moves`;
-    if (minute < 1) {
-      winningTime = `${second} seconds`;
-    } else if (minute === 1) {
-      winningTime = `${minute} minute & ${second} seconds`;
+// TIMER
+function startTimer() {
+  interval = setInterval(function () {
+    if (second < 10) {
+      timer.innerHTML = minute + ":0" + second;
+    } else {
+      timer.innerHTML = minute + ":" + second;
     }
-    pTime.innerHTML = winningTime;
-    pMoves.innerHTML = winningMoves;
-    let winWindow = document.querySelector(".winWindow");
-    setTimeout(() => {
-      winWindow.style.display = "flex";
-    }, 1500);
-  } else {
-    return;
-  }
+    second++;
+    if (second === 60) {
+      minute++;
+      second = 0;
+    }
+    if (minute === 60) {
+      hour++;
+      minute = 0;
+    }
+  }, 1000);
 }
